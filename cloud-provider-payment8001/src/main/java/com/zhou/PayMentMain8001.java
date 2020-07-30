c6562c1ad21dc0f9338efa0b71320012a979a6d2
springboot-crud <930265645@qq.com>
2020-07-30 11:23:10 +0800

    添加扫描dao层包注解

--- cloud-provider-payment8001/src/main/java/com/zhou/PayMentMain8001.java
+++ cloud-provider-payment8001/src/main/java/com/zhou/PayMentMain8001.java
@@ -1,8 +1,11 @@
 package com.zhou;
 
+import org.mybatis.spring.annotation.MapperScan;
+import org.mybatis.spring.annotation.MapperScans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@MapperScan("com.zhou.dao")
 @SpringBootApplication
 public class PayMentMain8001 {
     public static void main(String[] args) {
