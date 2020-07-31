1b75aaa1ddbb6c2f5789c73b0e0aad3948005789
springboot-crud <930265645@qq.com>
2020-07-31 16:07:02 +0800

    修改

--- cloud-conusumer-order80/src/main/java/com/zhou/OrderMain80.java
+++ cloud-conusumer-order80/src/main/java/com/zhou/OrderMain80.java
@@ -2,9 +2,8 @@ package com.zhou;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude= {DataSourceAutoConfiguration.class})
+@SpringBootApplication
 public class OrderMain80 {
     public static void main(String[] args) {
         SpringApplication.run(OrderMain80.class, args);
